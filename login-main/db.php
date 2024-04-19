862e159e0615b820e26e411b759c9925a334ed3b
ASTUDILLO-Victor <167276606+ASTUDILLO-Victor@users.noreply.github.com>
2024-04-18 22:05:10 -0500

    actualizacio

---
+++ login-main/db.php
@@ -0,0 +1,7 @@
+<?php
+
+$conexion=mysqli_connect("localhost","root","","id21026606_bd_esp8266");
+
+
+?>
+<H1>CONECTADO</H1>
---
+++ login-main/validar.php
@@ -0,0 +1,30 @@
+<?php
+include('db.php');
+$usuario=$_POST['usuario'];
+$contraseña=$_POST['contraseña'];
+session_start();
+$_SESSION['usuario']=$usuario;
+
+
+$conexion=mysqli_connect("localhost","root","","id21026606_bd_esp8266");
+
+$consulta="SELECT*FROM usuario where nombre='$usuario' and clave='$contraseña'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+  
+    header("location:home.php");
+
+}else{
+    ?>
+    <?php
+    include("index.html");
+
+  ?>
+  <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
+  <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
